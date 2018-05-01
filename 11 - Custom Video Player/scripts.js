8f7a204dadb909f3ce6a39cdbc3234bec6d637c0
Forty Morgan <dimazhmudikov@gmail.com>
2018-05-01 22:08:41 +0300

    Add skip time function

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -3,14 +3,19 @@ const player = document.querySelector('.player');
 const controls = {
   toggle: player.querySelector('.toggle'),
   video: document.querySelector('.viewer'),
+  skip: document.querySelectorAll('[data-skip]'),
 };
 
 const videoPlayPause = () => {
   controls.video[controls.video.paused ? 'play' : 'pause']();
-}
+};
 
 const updateButton = () => {
   controls.toggle.innerText = controls.video.paused ? '►' : '❚❚';
+};
+
+const skipTime = event => {
+  controls.video.currentTime += +event.target.dataset.skip;
 }
 
 controls.toggle.addEventListener('click', videoPlayPause);
@@ -18,3 +23,5 @@ controls.video.addEventListener('click', videoPlayPause);
 
 controls.video.addEventListener('play', updateButton);
 controls.video.addEventListener('pause', updateButton);
+
+controls.skip.forEach(button => button.addEventListener('click', skipTime))
