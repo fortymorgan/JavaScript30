4d6c3322ea7748706807a76379b64dba97b39ab8
Forty Morgan <dimazhmudikov@gmail.com>
2018-05-01 22:16:47 +0300

    Add volume and playbackRate update function

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -3,7 +3,8 @@ const player = document.querySelector('.player');
 const controls = {
   toggle: player.querySelector('.toggle'),
   video: document.querySelector('.viewer'),
-  skip: document.querySelectorAll('[data-skip]'),
+  skipButtons: document.querySelectorAll('[data-skip]'),
+  sliders: document.querySelectorAll('.player__slider'),
 };
 
 const videoPlayPause = () => {
@@ -16,7 +17,11 @@ const updateButton = () => {
 
 const skipTime = event => {
   controls.video.currentTime += +event.target.dataset.skip;
-}
+};
+
+const rangeUpdate = event => {
+  controls.video[event.target.name] = event.target.value;
+};
 
 controls.toggle.addEventListener('click', videoPlayPause);
 controls.video.addEventListener('click', videoPlayPause);
@@ -24,4 +29,6 @@ controls.video.addEventListener('click', videoPlayPause);
 controls.video.addEventListener('play', updateButton);
 controls.video.addEventListener('pause', updateButton);
 
-controls.skip.forEach(button => button.addEventListener('click', skipTime))
+controls.skipButtons.forEach(button => button.addEventListener('click', skipTime));
+
+controls.sliders.forEach(slider => slider.addEventListener('input', rangeUpdate));
